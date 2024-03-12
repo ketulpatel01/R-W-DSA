375ba0dd2b385f523f9a38370ad99d2d17ce2a6c
ketul <ketul3168@gmail.com>
2024-03-12 16:26:05 +0530

    lecture 6

---
+++ Lecture 6/lect6_1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // practical 1
+    for (int i = 1; i <=5; i++)
+    {
+        for(int j=1; j<=5; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+    
+
+    return 0;
+}
